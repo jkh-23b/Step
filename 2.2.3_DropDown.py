c4e4b27875d85391405d52c4c2c7fcedb9f3d1b8
Khabibullev <khabibullaev023@gmail.com>
2024-08-14 10:34:25 +0500

    2.2.3_DropDown.py

--- 2.2.3_DropDown.py
+++ 2.2.3_DropDown.py
@@ -1,5 +1,6 @@
 import time
 import math
+from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
