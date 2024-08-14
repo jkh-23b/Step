73257a9da15fcea4572675599b078192825a11d9
Khabibullev <khabibullaev023@gmail.com>
2024-08-14 11:29:01 +0500

    testing git pull

--- 2.2.3_DropDown.py
+++ 2.2.3_DropDown.py
@@ -1,7 +1,6 @@
 import time
 import math
 from selenium import webdriver
-import os
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
