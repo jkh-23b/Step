414b20d9c149501618218534f9a45bfe94143a34
Khabibullev <khabibullaev023@gmail.com>
2024-08-14 11:19:15 +0500

    Testing merge command

--- 2.2.3_DropDown.py
+++ 2.2.3_DropDown.py
@@ -1,6 +1,7 @@
 import time
 import math
 from selenium import webdriver
+import os
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
